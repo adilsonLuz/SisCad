8e4806fa6b9c73a140b75abeba63669a1b808fd3
Adilson Luz <adilson.luz@fatec.sp.gov.br>
2024-06-11 14:46:58 -0300

    Colors

---
+++ test/Cores.java
@@ -0,0 +1,18 @@
+/**
+ * Classe criada para Armazenar as cores do projeto
+ * @author Adilson Luz
+ * @since Classe Criada em 11/06/2024, 14:25:25
+ */
+public class Cores {
+    
+    //Vermelho claro #da251c rgba(218,37,28,255)
+    //Vermelho escuro #8c2425 rgba(159,37,36,255)
+    //violeta #424674 rgb(66,70,116)
+    //Azul Escuro #202964 rgba(32,41,100,255)
+    //Azul Claro #b5b5c1 rgba(181,181,193,255)
+    //Verde Escuro #0e4b29 rgba(14,75,41,255)
+    //Verde Claro #0a7235 rgba(10,114,53,255)
+    
+    
+
+}//fim da classe
